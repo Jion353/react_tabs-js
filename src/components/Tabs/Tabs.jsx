0bd3d22c73dbcf1ca58c170a1de72b2dde39b43c
Bovt Dmytro <mr.bovt2004@gmail.com>
2024-11-28 10:39:23 +0200

    done task

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, text, setText }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li className="is-active" data-cy="Tab" key={tab.id}>
+              <a href="#tab-1" data-cy="TabLink" onClick={() => setText(tab)}>
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {text.content}
+      </div>
+    </div>
+  );
+};
